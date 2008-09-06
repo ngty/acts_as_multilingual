87d789fcece32300673c05c78984ef168f940d6d
ngtzeyang <ngty77@gmail.com>
2008-09-06 13:45:55 +0800

    Added specs for testing: 1). non-multilingual entity, 2). multilingual? w.r.t to class (in addition to the original instance), 3). customizing of foreign key for lang alias.

--- spec/acts_as_multilingual_spec.rb
+++ spec/acts_as_multilingual_spec.rb
@@ -1,37 +1,42 @@
-# $Id: NgTzeYang [nineone@singnet.com.sg] 04 Sep 2008 21:28 $
+# $Id: NgTzeYang [nineone@singnet.com.sg] 06 Sep 2008 13:34 $
 # 
 
 require File.dirname(__FILE__) + '/spec_helper'
 
 
-class MultilingualEntity < ActiveRecord::Base
+class MultilingualFirstTestEntity < ActiveRecord::Base
   acts_as_multilingual :actions => :archive
   def archive
     update_attribute( :status, 'archived' ) and self
   end
 end 
 
+class MultilingualSecondTestEntity < ActiveRecord::Base
+  acts_as_multilingual :foreign_key => :alternate_alias_id
+end 
+
+class NonMultilingualTestEntity < ActiveRecord::Base
+end
+
 
 describe "Acting as multilingual" do
 
-  fixtures :multilingual_entities
+  fixtures :multilingual_first_test_entities
+  fixtures :multilingual_second_test_entities
 
   before(:each) do
-    @entity = MultilingualEntity.new
+    @entity = MultilingualFirstTestEntity.new
   end
 
-  def lang_entities( *args )
-    entities = [ args ].flatten.collect { | name | multilingual_entities(name) }
+  def lang_entities(*args)
+    entities = [args].flatten.collect do |name| 
+      block_given? ? yield(name) : multilingual_first_test_entities(name) 
+    end
     entities.length == 1 ? entities.first : entities
   end
 
   def equal_entities?( entities_1, entities_2 )
     ( entities_1.length == entities_2.length ) and ( entities_1 - entities_2 ).empty?
-  end
-
-
-  it "should identify itself as multilingual" do
-    @entity.should be_multilingual
   end
 
   
@@ -89,12 +94,12 @@ describe "Acting as multilingual" do
     [ :new_with_lang_aliases, :build_with_lang_aliases ].each do | meth |
 
       it "using #{meth.to_s}, should build entity with the 1st set of attrs and return entity" do
-        entity = MultilingualEntity.send( meth, @entities_attrs )
+        entity = MultilingualFirstTestEntity.send( meth, @entities_attrs )
         entity.lang.should == @langs[0]
       end
 
       it "using #{meth.to_s}, should build entity's aliases with remaining sets of attrs" do
-        entity = MultilingualEntity.send( meth, @entities_attrs )
+        entity = MultilingualFirstTestEntity.send( meth, @entities_attrs )
         equal_entities?( entity.lang_aliases.collect { |a| a.lang }, @langs[1,(@langs.size-1)] ).should be_true
       end
 
@@ -175,7 +180,47 @@ describe "Acting as multilingual" do
   end
 
 
+  describe "when declared as multilingual" do
+    
+    def other_lang_entities(*args)
+      lang_entities(*args) { |name| multilingual_second_test_entities(name) }
+    end
+
+    it "should support customizing of lang alias foreign key" do
+      entity, entity_aliases = other_lang_entities(:en_SG), other_lang_entities( :zh_CN, :en_US )
+      equal_entities?( entity.lang_aliases, entity_aliases ).should be_true
+    end
+
+  end
+
+
+  it "entity should identify itself as multilingual" do
+    @entity.should be_multilingual
+  end
+
+  it "class should identify itself as multilingual" do
+    @entity.class.should be_multilingual
+  end
+
+  
 end 
+
+
+describe "Acting as non-multilingual" do
+
+  before(:each) do
+    @entity = NonMultilingualTestEntity.new
+  end
+
+  it "entity should not identify itself as multilingual" do
+    @entity.should_not be_multilingual
+  end
+
+  it "class should not identify itself as multilingual" do
+    @entity.class.should_not be_multilingual
+  end
+
+end
 
 
 # __END__
